8b1089ea87461e6def7d5a437460028b8557a2fc
shuaibinliu <13588046135@163.com>
2020-10-30 06:03:40 +0800

    异步任务完善。待测试

--- jinrui/extensions/tasks.py
+++ jinrui/extensions/tasks.py
@@ -35,23 +35,65 @@ def cancel_async_task(conn_id):
 
 
 def auto_setpic():
-    from jinrui.models import j_paper
+    from jinrui.models import j_paper, j_question
     import requests
-    jplist = j_paper.query.filter(j_paper.encode_tag == '0').all()
-    for jp in jplist:
-        if jp.doc_url:
+    import os
+    from datetime import datetime
+    from ..control.Cautopic import CAutopic
+    cp = CAutopic()
+    def _get_path(fold):
+        """获取服务器上文件路径"""
+        time_now = datetime.now()
+        year = str(time_now.year)
+        month = str(time_now.month)
+        day = str(time_now.day)
+        filepath = os.path.join(current_app.config['BASEDIR'], 'img', fold, year, month, day)
+        # file_db_path = os.path.join('/img', fold, year, month, day)
+        if not os.path.isdir(filepath):
+            os.makedirs(filepath)
+        return filepath
 
-            data = requests.get(jp.doc_url)
-            filename = str(jp.doc_url).split('/')[-1]
-            # 拉取远端文件
-            with open(filename, 'wb') as head:
-                head.write(data.content)
-            pass
-            # todo 添加文档解析
-        if jp.answer_doc_url:
-            # todo 添加文档解析
-            pass
-    pass
+    def _get_fetch(path, qiniu=False):
+        # if qiniu:
+        #     content = requests.get(MEDIA_HOST + path)
+        # else:
+        content = requests.get(path)
+
+        filename = cp.random_name('.pdf')
+
+        filepath = _get_path('pdf')
+        # filedbname = os.path.join(filedbpath, filename)
+        filename = os.path.join(filepath, filename)
+        with open(filename, 'wb') as head:
+            head.write(content.content)
+        return filename
+
+    jplist = j_paper.query.filter(j_paper.encode_tag == '0').all()
+
+    try:
+        with db.auto_commit():
+            update_list = []
+            for jp in jplist:
+                paper_dict = {}
+                answer_dict = {}
+                if jp.doc_url:
+                    doc_path = _get_fetch(jp.doc_url)
+                    paper_dict = cp.transfordoc(doc_path)
+
+                if jp.answer_doc_url:
+                    answer_path = _get_fetch(jp.answer_doc_url)
+                    answer_dict = cp.transfordoc(answer_path)
+                for paper_num in paper_dict:
+                    question = j_question.query.filter(
+                        j_question.paper_id == jp.id, j_question.question_number == paper_num).first()
+                    if not question:
+                        continue
+                    question.update({'content': paper_dict.get(paper_num), 'answer': answer_dict.get(paper_num)})
+                    update_list.append(question)
+            db.session.add_all(update_list)
+    except Exception as e:
+        current_app.logger.info('解析试卷失败 {}'.format(e))
+
 
 
 if __name__ == '__main__':
